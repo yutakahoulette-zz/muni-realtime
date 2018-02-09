8d7501ae752e109a1b2cb449883d1a1bfb535eba
Yutaka Houlette <yutakahoulette@gmail.com>
2018-02-09 11:52:51 -0800

    Render static map elements

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,3 +1,3 @@
-const path = (dir, fileName) => `maps/${dir}/${fileName}.json`
+const path = (dir, fileName) => `public/maps/${dir}/${fileName}.json`
 const log = (action, fileName) => console.log(`${action} ${fileName}`)
 module.exports = {log, path}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3'
 
 class App extends Component {
+  state = {
+    staticMapElements: ['freeways', 'arteries', 'streets', 'neighborhoods'],
+    neighborhoods: [],
+    streets: [],
+    freeways: [],
+    arteries: [],
+    chartWidth: 800,
+    chartHeight: 790
+  }
+  componentDidMount() {
+    this.state.staticMapElements.forEach(name => {
+      d3.json(`/maps/simplified/${name}.json`, (err, resp) => {
+        if (err) throw err
+        this.setState({[name]: resp.features})
+      })
+    })
+  }
+  projection(d) {
+    const sfCoords = {
+      lat: 37.7749,
+      long: 122.4194
+    }
+    const projection = d3.geoAlbers()
+      .translate([this.state.chartWidth / 2, this.state.chartHeight / 2])
+      .scale(270000)
+      .rotate([sfCoords.long, 0.004])
+      .center([-0.015, sfCoords.lat])
+    return d3.geoPath().projection(projection)(d)
+  }
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <svg width={ this.state.chartWidth } 
+           height={ this.state.chartHeight } 
+           viewBox={`0 0 ${this.state.chartWidth} ${this.state.chartHeight}`}>
+        {
+          this.state.staticMapElements.map((name, i) => (
+            <g className={name} key={ `group-${name}-${ i }` }>
+              {
+                this.state.streets.map((d,ii) => (
+                  <path
+                    key={ `path-${name}-${ ii }` }
+                    d={ this.projection(d) }
+                  />
+                ))
+              }
+            </g>
+          ))
+        }
+      </svg>
+    )
   }
 }
 
