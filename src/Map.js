b64dbff9535538f8d573236252fba8c75d1eacdd
Yutaka Houlette <yutakahoulette@gmail.com>
2018-02-09 23:33:26 -0800

    Get vehicles correctly showing

--- src/Map.js
+++ src/Map.js
@@ -1,49 +1,41 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react'
 import * as d3 from 'd3'
 
-class Map extends PureComponent {
+class Map extends Component {
   state = {
-    staticMapElements: ['neighborhoods'],
     neighborhoods: [],
-    streets: [],
-    freeways: [],
-    arteries: [],
     routes: {},
     chartWidth: 800,
-    chartHeight: 790
+    chartHeight: 750
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const getLocation = () => {
-      const time = (new Date()).getTime() - 15000
-      const url = (time) => `http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&t=${time}`
-      d3.json(url(time), (resp) => {
+      const url = () => `http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&t=${(new Date()).getTime() - 15000}`
+      d3.json(url(), (resp) => {
         if (!resp.vehicle) return
         resp.vehicle.forEach(v => {
           const vehicle = [v.lon, v.lat].map(Number)
           const routes = {...this.state.routes}
-          routes[v.routeTag].vehicle = vehicle
+          const vehicles = routes[v.routeTag].vehicles
+          routes[v.routeTag].vehicles = (vehicles && vehicles.length)
+            ? vehicles.concat([vehicle]) 
+            : [vehicle]
           this.setState({routes})
         })
       })
-    }
-    const tick = () => {
       window.setTimeout(() => {
         getLocation()
-        tick()
       }, 15000)
     }
-    this.state.staticMapElements.forEach(name => {
-      d3.json(this.mapPath(name), (err, resp) => {
-        if (err) throw err
-        this.setState({[name]: resp.features})
-      })
+    d3.json(this.mapPath('neighborhoods'), (err, resp) => {
+      if (err) throw err
+      this.setState({neighborhoods: resp.features})
     })
     d3.json(this.mapPath('routes'), (err, resp) => {
       if (err) throw err
       this.setState({routes: resp})
       getLocation()
-      tick()
     })
   }
 
@@ -58,69 +50,106 @@ class Map extends PureComponent {
     }
     return d3.geoAlbers()
       .translate([this.state.chartWidth / 2, this.state.chartHeight / 2])
-      .scale(270000)
-      .rotate([sfCoords.lon, 0.004])
-      .center([-0.015, sfCoords.lat])
+      .scale(280000)
+      .rotate([sfCoords.lon, 0.003])
+      .center([-0.02, sfCoords.lat])
+  }
+
+  toggleRoute(routeName) {
+    const routes = {...this.state.routes}
+    const route = routes[routeName]
+    route.hidden = !route.hidden
+    routes[routeName] = route
+    this.setState({routes})
+  }
+
+  toggleAllRoutes() {
+    Object.keys(this.state.routes).forEach((routeName) => this.toggleRoute(routeName))
   }
 
   render() {
     return (
-      <svg width={ this.state.chartWidth } 
-           height={ this.state.chartHeight } >
-        {
-          this.state.staticMapElements.map((name, i) => (
-            <g className={name} key={ `${name}-${ i }` }>
+      <div className='container'>
+        <h1>Muni realtime</h1>
+        <svg width={ this.state.chartWidth } 
+            height={ this.state.chartHeight } >
+          {
+            <g className='neighborhoods'>
               {
-                this.state[name].map((d,ii) => (
+                this.state.neighborhoods.map((d,ii) => (
                   <path
-                    key={ `${name}-path-${ ii }` }
+                    key={ `neighborhoods-path-${ ii }` }
                     d={ d3.geoPath(this.projection())(d) }
                   />
                 ))
               }
             </g>
-          ))
-        }
-        {
-          Object.keys(this.state.routes).map((key, i) => {
-            const data = this.state.routes[key]
-            return !data.features || data.hide
-              ? <g key={ `${key}-route-${ i }` }/>
-              : <g className='route' key={ `${key}-route-${ i }` }>
-                  {
-                    data.features.map((f,ii) => (
-                      <path
-                        key={ `${key}-route-path-${ ii }` }
-                        d={ d3.geoPath(this.projection())(f) }
-                        stroke={`#${data.color}`}
-                      />
-                    ))
-                  }
-                </g>
-          })
-        }
-        {
-          Object.keys(this.state.routes).map((key, i) => {
-            const data = this.state.routes[key]
-            return !data.vehicle || data.hide
-              ? <g key={ `${key}-vehicle-${ i }` }/>
-              : <g className='vehicles' key={ `${key}-vehicles-${ i }` }>
-                  {
-                    data.vehicle.map((v,ii) => (
-                      <circle
-                        key={ `${key}-vehicle-path-${ ii }` }
-                        d={ d3.geoPath(this.projection())(v) }
-                        r='4'
-                        fill={`#${data.color}`}
-                        cx={this.projection()(data.vehicle)[0]}
-                        cy={this.projection()(data.vehicle)[1]}
-                      />
-                    ))
-                  }
-                </g>
-          })
-        }
-      </svg>
+          }
+          {
+            Object.keys(this.state.routes).map((key, i) => {
+              const data = this.state.routes[key]
+              return !data.features || data.hidden
+                ? <g key={ `${key}-route-${ i }` }/>
+                : <g className='route' key={ `${key}-route-${ i }` }>
+                    {
+                      data.features.map((f,ii) => (
+                        <path
+                          key={ `${key}-route-path-${ ii }` }
+                          d={ d3.geoPath(this.projection())(f) }
+                          stroke={`#${data.color}`}
+                        />
+                      ))
+                    }
+                  </g>
+            })
+          }
+          {
+            Object.keys(this.state.routes).map((key, i) => {
+              const data = this.state.routes[key]
+              return !data.vehicles || data.hidden
+                ? <g key={ `${key}-vehicles-${ i }` }/>
+                : <g className='vehicles' key={ `${key}-vehicles-${ i }` }>
+                    {
+                      data.vehicles.map((v,ii) => {
+                        return (
+                            <circle
+                            key={ `${key}-vehicles-path-${ ii }` }
+                            r='4'
+                            fill={`#${data.color}`}
+                            cx={this.projection()(v)[0]}
+                            cy={this.projection()(v)[1]}
+                          />
+                        )
+                      })
+                    }
+                  </g>
+            })
+          }
+        </svg>
+        <div className='buttons'>
+          <div className='buttonWrapper'>
+            <button onClick={() => this.toggleAllRoutes()}>
+              Toggle all routes
+            </button>
+          </div>
+          {
+            Object.keys(this.state.routes).map((key, i) => {
+              const data = this.state.routes[key]
+              return (
+                <div className='buttonWrapper' key={`button-${i}`}>
+                  <button style={{
+                      borderColor: `#${data.color}`,
+                      opacity: data.hidden ? '0.2' : '1'
+                    }}
+                    onClick={() => this.toggleRoute(key)}>
+                    {key}
+                  </button>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     )
   }
 }
