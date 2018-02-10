384f44c35ee39b9ead578d5a7d43970b94398067
Yutaka Houlette <yutakahoulette@gmail.com>
2018-02-10 11:06:21 -0800

    Decouple vehicle state from route state

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,13 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import * as d3 from 'd3'
 
-class Map extends PureComponent {
+class Map extends Component {
   state = {
     neighborhoods: [],
     routes: {},
+    routesHidden: {},
+    vehicles: {},
+    vehiclesLoaded: false,
     chartWidth: 800,
     chartHeight: 750
   }
@@ -13,17 +16,19 @@ class Map extends PureComponent {
     const getLocation = () => {
       const url = () => `http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&t=${(new Date()).getTime() - 15000}`
       d3.json(url(), (resp) => {
+        const vehicles = {...this.state.vehicles}
         if (!resp.vehicle) return
         resp.vehicle.forEach(v => {
           const id = v.id
           const vehicle = [v.lon, v.lat].map(Number)
-          const routes = {...this.state.routes}
-          const vehicles = routes[v.routeTag].vehicles
-          if (!vehicles) {
-            routes[v.routeTag].vehicles = {}
+          if (!vehicles[v.routeTag]) {
+            vehicles[v.routeTag] = {}
           }
-          routes[v.routeTag].vehicles[id] = vehicle
-          this.setState({routes})
+          vehicles[v.routeTag][id] = vehicle
+        })
+        this.setState({
+          vehicles,
+          vehiclesLoaded: true,
         })
       })
       window.setTimeout(() => {
@@ -36,7 +41,14 @@ class Map extends PureComponent {
     })
     d3.json(this.mapPath('routes'), (err, resp) => {
       if (err) throw err
-      this.setState({routes: resp})
+      const routesHidden = Object.keys(resp).reduce((obj, key) => {
+        obj[key] = false
+        return obj
+      }, {})
+      this.setState({
+        routes: resp,
+        routesHidden
+      })
       getLocation()
     })
   }
@@ -58,15 +70,17 @@ class Map extends PureComponent {
   }
 
   toggleRoute(routeName) {
-    const routes = {...this.state.routes}
-    const route = routes[routeName]
-    route.hidden = !route.hidden
-    routes[routeName] = route
-    this.setState({routes})
+    const routesHidden = {...this.state.routesHidden}
+    routesHidden[routeName] = !routesHidden[routeName]
+    this.setState({routesHidden})
   }
 
   toggleAllRoutes() {
-    Object.keys(this.state.routes).forEach((routeName) => this.toggleRoute(routeName))
+    const routesHidden = {...this.state.routesHidden}
+    Object.keys(this.state.routesHidden).forEach((routeName) => {
+      routesHidden[routeName] = !routesHidden[routeName]
+    })
+    this.setState({routesHidden})
   }
 
   render() {
@@ -90,15 +104,16 @@ class Map extends PureComponent {
           {
             Object.keys(this.state.routes).map((key, i) => {
               const data = this.state.routes[key]
-              return !data.features || data.hidden
+              return !data.features
                 ? <g key={ `${key}-route-${ i }` }/>
-                : <g className='route' key={ `${key}-route-${ i }` }>
+                : <g className={`route ${key}`}  key={ `${key}-route-${ i }` }>
                     {
                       data.features.map((f,ii) => (
                         <path
                           key={ `${key}-route-path-${ ii }` }
                           d={ d3.geoPath(this.projection())(f) }
-                          stroke={`#${data.color}`}
+                          stroke={this.state.routesHidden[key] ? 'white' :`#${data.color}`}
+                          strokeOpacity={this.state.routesHidden[key] ? '0' : '0.5'}
                         />
                       ))
                     }
@@ -106,21 +121,24 @@ class Map extends PureComponent {
             })
           }
           {
-            Object.keys(this.state.routes).map((key, i) => {
-              const data = this.state.routes[key]
-              return !data.vehicles || data.hidden
+            Object.keys(this.state.vehicles).map((key, i) => {
+              const data = this.state.vehicles[key]
+              const color = this.state.routes[key].color
+              const hidden = this.state.routesHidden[key]
+              return !data 
                 ? <g key={ `${key}-vehicles-${ i }` }/>
                 : <g className='vehicles' key={ `${key}-vehicles-${ i }` }>
                     {
-                      Object.keys(data.vehicles).map((key,ii) => {
-                        const coords = this.projection()(data.vehicles[key])
+                      Object.keys(data).map((id,ii) => {
+                        const coords = this.projection()(data[id])
                         return (
                             <circle
-                            key={ `${key}-vehicles-path-${ ii }` }
+                            key={ `${id}-vehicles-path-${ ii }` }
                             r='4'
-                            fill={`#${data.color}`}
+                            fill={`#${color}`}
                             cx={coords[0]}
                             cy={coords[1]}
+                            fillOpacity={hidden ? '0' : '1'}
                           />
                         )
                       })
@@ -129,6 +147,7 @@ class Map extends PureComponent {
             })
           }
         </svg>
+        <p>{this.state.vehiclesLoaded ? '' : 'Loading vehicles...'}</p>
         <div className='buttons'>
           <div className='buttonWrapper'>
             <button onClick={() => this.toggleAllRoutes()}>
@@ -142,7 +161,7 @@ class Map extends PureComponent {
                 <div className='buttonWrapper' key={`button-${i}`}>
                   <button style={{
                       borderColor: `#${data.color}`,
-                      opacity: data.hidden ? '0.2' : '1'
+                      opacity: this.state.routesHidden[key] ? '0.2' : '1'
                     }}
                     onClick={() => this.toggleRoute(key)}>
                     {key}
